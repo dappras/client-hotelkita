947d5ab65d1d9da6640f676ff1a5ee952cce4c6a
daffa.rasyid <daffa.naufan@gmail.com>
2022-12-07 12:03:27 +0700

    update

--- src/utils/http.js
+++ src/utils/http.js
@@ -4,7 +4,7 @@ import Cookies from "universal-cookie";
 const cookies = new Cookies()
 
 const http = axios.create({
-    baseURL: process.env.REACT_APP_URL_API,
+    baseURL: 'http://103.226.139.23:3000/api',
     headers: {
         "auth-token": cookies.get("token"),
     },
